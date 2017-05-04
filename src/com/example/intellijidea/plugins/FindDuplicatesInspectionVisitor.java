d03ae4f8dc375431824edcb5ef889a8e71ac36a0
niklsss <nik_alekseev@outlook.com>
2017-05-04 14:16:27 +0300

    update

--- src/com/example/intellijidea/plugins/FindDuplicatesInspectionVisitor.java
+++ src/com/example/intellijidea/plugins/FindDuplicatesInspectionVisitor.java
@@ -68,9 +68,6 @@ public class FindDuplicatesInspectionVisitor extends JavaElementVisitor {
             }
 
         }
-        else{
-            
-        }
     }
 
     private static class ShowDiffFix implements LocalQuickFix {
